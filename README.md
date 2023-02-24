227ede04d1a073b26e849896bef8812eff230047
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2023-02-23 21:36:47 -0300

    Se agrego un ejemplo con async-await y fetch

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 La idea de este repositorio es simplemente mostrar como se puede utilizar Google Sheets como una API para proyectos.
 
-Tanto en el código como aquí dejo algunos comentarios con lo más importante para entender que se necesita y como se realiza, aprendido a partir del video de Gonzalo Pozzo (Goncy), en el cual a partir del minuto 38:45 se hace la conexión con Google Sheets: https://www.youtube.com/watch?v=DgPcpte1eoA
+Tanto en el código como aquí dejo algunos comentarios con lo más importante para entender que se necesita y como se realiza, aprendido a partir de los videos de Gonzalo Pozzo (Goncy):  
+https://www.youtube.com/watch?v=DgPcpte1eoA  
+https://www.youtube.com/watch?v=kPGLyOUrlw8
 
-El archivo de Google Sheets que utilice como ejemplo es:
+El archivo de Google Sheets que utilice como ejemplo es:  
 https://docs.google.com/spreadsheets/d/1dOTZj-h3piakOaVgbSr_-NgbBqcX2Ksf_9q26jdlhxE/edit#gid=0
 
 ### Librerias utilizadas:
@@ -17,7 +19,7 @@ https://docs.google.com/spreadsheets/d/1dOTZj-h3piakOaVgbSr_-NgbBqcX2Ksf_9q26jdl
 1. Para empezar, en el archivo de Google Sheets debemos ir a "Archivo" - "Compartir" - "Publicar en la web" y elegir "Valores separados por coma" (.csv).
    Luego de eso el link que se nos proporciona es el endpoint al que se realiza la consulta.
 
-2. El segundo paso es instalar una libreria que nos permita hacer un request, que en este caso se uso "axios".
+2. El segundo paso es instalar una libreria que nos permita hacer un request, que en este caso se uso "axios", pero también se lo realizó con async-await y fetch, así que en el código se muestran las dos opciones.
 
 3. En tercer lugar, hay que instalar una libreria para parsear csv a json, que en este caso usamos PapaParse, ya que Google Sheets nos devolverá la información en csv pero nosotros la queremos en json para luego manipularla y mostrarla en pantalla.
 
--- src/App.jsx
+++ src/App.jsx
@@ -9,33 +9,62 @@ function App() {
 	const API_GOOGLE_SHEETS =
 		"https://docs.google.com/spreadsheets/d/e/2PACX-1vQRMhbwxsY1gncMQlaeRuO6WEe9HL51SJOzULDHiRQmROzykZesGp_X5VK8icDzphU2vCA2kopXQZNl/pub?output=csv";
 
-	const getProducts = async () => {
-		axios
-			.get(API_GOOGLE_SHEETS, {
-				responseType: "blob", // se le debe indicar que es de tipo blob
-			})
-			.then((response) => {
-				// uso el constructor de promesas para crear una, porque el metodo de papa.parse no funciona con promise, asi creamos una promesa a nuestro gusto
-				return new Promise((resolve, reject) => {
-					// a papaparse le pasamos response.data que es la info recibida en csv
-					Papa.parse(response.data, {
-						header: true, // le indica a papaparse que el primer elemento del array son los headers (titulos) de la tabla, con eso nos va a armar el json poniendo esos valores como claves
-						complete: (results) => {
-							// "complete" es una funcion que en "results.data" tiene los resultados de parsear el objeto
-							//console.log(results.data);
-							setProducts(results.data);
-							return resolve(results.data); // resolve del promise
-						},
-						error: (error) => {
-							return reject(error.message); // reject del promise si hay un error
-						},
-					});
-				});
+	// CONSULTA CON ASYNC-AWAIT Y FETCH
+	async function getData() {
+		const res = await fetch(API_GOOGLE_SHEETS);
+		const data = await res.text(); // lo obtenemos como texto plano en lugar de json
+		// usamos la libreria papaparse para convertir de csv a json
+		// uso el constructor de promesas para crear una, porque el metodo de papa.parse no funciona con promise, asi creamos una promesa a nuestro gusto
+		const dataParsed = await new Promise((resolve, reject) => {
+			// a papaparse le pasamos los datos recibidos que queremos pasar de csv a json
+			Papa.parse(data, {
+				header: true, // le indica a papaparse que el primer elemento del array son los headers (titulos) de la tabla, con eso nos va a armar el json poniendo esos valores como claves
+				// "complete" es una funcion que en "results.data" tiene los resultados de parsear el objeto
+				complete: (results) => {
+					resolve(results.data); // resolve del promise (el valor que devuelve si todo sale bien)
+					//si quisiera podria usar results.data aca, pero mejor hacerlo aparte:
+					//console.log(results.data);
+					//setProducts(results.data);
+				},
+				error: (error) => {
+					return reject(error.message); // reject del promise si hay un error
+				},
 			});
+		});
+		setProducts(dataParsed); // podria usarlo aca pero mejor aparte
+		return dataParsed;
+	}
+
+	// CONSULTA CON AXIOS
+	// const getData = async () => {
+	// 	axios
+	// 		.get(API_GOOGLE_SHEETS, {
+	// 			responseType: "blob", // se le debe indicar que es de tipo blob
+	// 		})
+	// 		.then((response) => {
+	// 			return new Promise((resolve, reject) => {
+	// 				Papa.parse(response.data, {
+	// 					header: true, // le indica a papaparse que el primer elemento del array son los headers (titulos) de la tabla, con eso nos va a armar el json poniendo esos valores como claves
+	// 					complete: (results) => {
+	// 						setProducts(results.data);	// lo hice aca dentro pero en la de fetch lo hice afuera usando la funcion getProducts
+	// 						return resolve(results.data); // resolve del promise
+	// 					},
+	// 					error: (error) => {
+	// 						return reject(error.message); // reject del promise si hay un error
+	// 					},
+	// 				});
+	// 			});
+	// 		});
+	// };
+
+	const getProducts = async () => {
+		const dataProducts = await getData();
+		setProducts(dataProducts);
 	};
 
 	useEffect(() => {
-		getProducts();
+		// getData();	// si haria el setState dentro de la funcion (como hice con axios para probar)
+		getProducts(); // si uso una funcion aparte para el setState
 	}, []);
 
 	return (
